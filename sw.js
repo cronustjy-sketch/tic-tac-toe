23b4e2a40866a7061d48d67c0399e9bb37d0f11d
cronustjy-sketch <cronustjy@gmail.com>
2026-06-04 15:01:27 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE='cronus-games-v1';
+var ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
+
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.map(function(k){ if(k!==CACHE) return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',function(e){
+  var url=e.request.url;
+  if(url.indexOf('workers.dev')!==-1 || url.indexOf('piesocket')!==-1){ return; }
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      return cached || fetch(e.request).then(function(resp){
+        return resp;
+      }).catch(function(){ return caches.match('/index.html'); });
+    })
+  );
+});
