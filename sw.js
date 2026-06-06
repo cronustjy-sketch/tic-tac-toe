011b536020fef60bbdb902352db39cebbe4478f5
cronustjy-sketch <cronustjy@gmail.com>
2026-06-06 11:26:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='cronus-games-v2';
+var CACHE='cronus-games-v3';
 var ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
 
 self.addEventListener('install',function(e){
